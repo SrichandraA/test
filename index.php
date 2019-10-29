6e46b62299f2adb4e614c64a57179e41e65f7f71
srichandra <srichandra.amrutaluri@riktamtech.com>
2019-10-29 13:18:43 +0530

    reverting

--- index.php
+++ index.php
@@ -4,18 +4,4 @@
 <p>Hi Hello</p>
 <p>Hi Hello</p>
 <p>Hi Hello</p>
-<div>
-    <p>world</p>
-    <p>world</p>
-    <p>world</p>
-    <p>world</p>
-    <p>world</p>
-</div>
-<div>
-    <p>hello world</p>
-    <p>world</p>
-    <p>world</p>
-    <p>world</p>
-    <p>world</p>
-</div>
 </html>
